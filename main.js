a7066e48e14ac6ab4cfc90d5be959fb138b9e1db
Ihor <ihor.i.baklan@globallogic.com>
2023-03-05 15:01:50 +0200

    Created test videochat

---
+++ main.js
@@ -0,0 +1,170 @@
+let APP_ID = "22479cba54144c0a93109eaa0dbe6396"
+
+let tocken = null
+let uid = String(Math.floor(Math.random() * 1000000000))
+
+let client, channel
+
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
+
+if (!roomId) {
+    window.location = 'lobby.html'
+}
+
+let localStream, remoteStream, peerConnection;
+const servers = { 'iceServers': [{ urls: ['stun:stun1.l.google.com:19302', "stun:stun2.l.google.com:19302"] }] };
+
+let constraints = {
+    video: {
+        width: { min: 640, ideal: 1280, max: 1920 },
+        height: { min: 480, ideal: 720, max: 1080 },
+    },
+    audio: true
+}
+
+let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({ uid, tocken })
+
+    channel = client.createChannel(roomId)
+    await channel.join()
+
+    channel.on("MemberJoined", handleUserJoined)
+    channel.on("MemberLeft", handleUserLeft)
+    client.on("MessageFromPeer", handleMessageFromPeer)
+
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    document.getElementById('user-1').srcObject = localStream;
+}
+
+const handleUserLeft = async (memberId) => {
+    document.getElementById('user-2').style.display = 'none';
+    document.getElementById('user-1').classList.remove("smallFrame");
+}
+
+const handleUserJoined = async (memberId) => {
+    createOffer(memberId);
+}
+
+const handleMessageFromPeer = async (message, memberId) => {
+    message = JSON.parse(message.text);
+
+    if (message.type === "offer") {
+        createAnswer(memberId, message.offer);
+    }
+
+    if (message.type === "answer") {
+        addAnswer(message.answer);
+    }
+
+    if (message.type === "candidate") {
+        if (peerConnection) {
+            peerConnection.addIceCandidate(message.candidate);
+        }
+    }
+}
+
+const createPeerConnection = async (memberId) => {
+    peerConnection = new RTCPeerConnection(servers);
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+    document.getElementById('user-2').style.display = 'block';
+
+    document.getElementById('user-1').classList.add("smallFrame");
+
+    if (!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        document.getElementById('user-1').srcObject = localStream;
+    }
+
+    localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach(track => {
+            remoteStream.addTrack(track);
+        });
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate) {
+            client.sendMessageToPeer({
+                text:
+                    JSON.stringify({ type: "candidate", candidate: event.candidate })
+            }, memberId)
+        }
+    }
+
+    return peerConnection;
+}
+
+const createOffer = async (memberId) => {
+    await createPeerConnection(memberId);
+
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    client.sendMessageToPeer({
+        text:
+            JSON.stringify({ type: "offer", offer })
+    }, memberId)
+}
+
+const createAnswer = async (memberId, offer) => {
+    await createPeerConnection(memberId);
+
+    await peerConnection.setRemoteDescription(offer);
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    client.sendMessageToPeer({
+        text:
+            JSON.stringify({ type: "answer", answer })
+    }, memberId)
+}
+
+const addAnswer = async (answer) => {
+    if (!peerConnection.currentRemoteDescription) {
+        await peerConnection.setRemoteDescription(answer);
+    }
+}
+
+const leaveChannel = async () => {
+    await channel.leave();
+    await client.logout();
+}
+
+const toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(track => track.kind === 'video');
+
+    if (videoTrack.enabled) {
+        videoTrack.enabled = false;
+        document.getElementById('camera-btn').style.background = 'rgb(255, 80, 80)';
+    } else {
+        videoTrack.enabled = true;
+        document.getElementById('camera-btn').style.background = 'rgb(179, 102, 249, .9)';
+    }
+}
+
+const toggleMic = async () => {
+    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio');
+
+    if (audioTrack.enabled) {
+        audioTrack.enabled = false;
+        document.getElementById('mic-btn').style.background = 'rgb(255, 80, 80)';
+    } else {
+        audioTrack.enabled = true;
+        document.getElementById('mic-btn').style.background = 'rgb(179, 102, 249, .9)';
+    }
+}
+
+window.addEventListener("beforeunload", leaveChannel);
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera);
+document.getElementById('mic-btn').addEventListener('click', toggleMic);
+
+init()
